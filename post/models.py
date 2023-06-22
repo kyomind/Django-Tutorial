de15d1b3643f9af01726b5e5569f787096dcbd3e
kyo <odinxp@gmail.com>
2023-06-22 18:47:18 +0800

    install app與新增model及遷移檔

--- post/models.py
+++ post/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+# 文章
+class Post(models.Model):
+    title = models.OneToOneField('Title', on_delete=models.CASCADE, related_name='post')
+    content = models.TextField()
+
+    def __str__(self):
+        return f'<{self.id}>: {self.title.main}'
+
+
+# 文章標題
+class Title(models.Model):
+    main = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+
+
+# 留言
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
