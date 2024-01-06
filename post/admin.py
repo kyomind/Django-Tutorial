7f958aac2f8765be608dd3cf1c578e65426b632d
kyo <odinxp@gmail.com>
2024-01-06 20:31:26 +0800

    refactor: refactor models and relationships for Post, Title, and Comment
    - Change the import of `Subtitle` in `post/admin.py`
    - Add a new migration file for altering the `Post` and `Title` models
    - Modify the `Post` model to have a `CharField` for the title instead of a `OneToOneField` relationship with `Title`
    - Delete the `Title` model
    - Add a new `Subtitle` model with a `CharField` for the content and a `OneToOneField` relationship with `Post`
    - Modify the `Post` model's `__str__` method to use the new `title` attribute instead of `title.main` and `title.subtitle`
    - Modify the `Title` model's `__str__` method to use the new `content` attribute instead of `main`
    - Modify the `Comment` model to have a `ForeignKey` relationship with `Post` instead of a `OneToOneField` relationship
    
    Signed-off-by: kyo <odinxp@gmail.com>

--- post/admin.py
+++ post/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from post.models import Comment, Post, Title
+from post.models import Comment, Post, Subtitle
 
 admin.site.register(Post)
-admin.site.register(Title)
+admin.site.register(Subtitle)
 admin.site.register(Comment)
